df3c69c8c5c84c1e6bce1bf1e386b2564e7e73d3
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-08-15 13:42:43 -0500

    Initial Commit

---
+++ experience-sampler/experience_samplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  experience_samplerApp.swift
+//  experience-sampler
+//
+//  Created by Darya Orlova on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct experience_samplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
