ff68f7496ef62c5de8396320412a5fcf10264c4f
dray-the-sea <109749397+dray-the-sea@users.noreply.github.com>
2022-08-17 12:26:50 -0500

    added the user input pieces

--- experience-sampler/Models/ModelData.swift
+++ experience-sampler/Models/ModelData.swift
@@ -6,6 +6,13 @@
 //
 
 import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    
+}
 
 
 func load<T: Decodable>(_ filename: String) -> T {
